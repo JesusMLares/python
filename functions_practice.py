a719c7f88aa61e7c8a3079394c37fa283f7d4203
JesusMLares <twinlares2@hotmail.com>
2024-08-17 09:23:54 -0700

    Initial commit and functions_practice complete

---
+++ functions_practice.py
@@ -0,0 +1,25 @@
+# 1. Function that prints hello
+# --------------------------
+# def hello():
+#     print("Hello")
+# hello()
+
+# 2. Packing List
+# ---------------
+# def pack(x, y, z):
+#     return[x, y, z]
+# print(pack('toothbrush', 'toothpaste', 'shampoo'))
+
+# 3. Eat Lunch
+# ------------
+# def eat_lunch(lunch_list):
+#     if not lunch_list:
+#         print("Your lunchbox is empty!")
+#     else:
+#         print(f"First I eat {lunch_list[0]}")
+#         for item in lunch_list[1:]: # [1:] Slices list and starts at index 1
+#             print(f"Next I eat {item}")
+
+# lunch_list = ["sandwich", "chips", "soda", "apple"]
+
+# eat_lunch(lunch_list)
